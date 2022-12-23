a7e6afda22fc8336efef2bf1a0eda278987c07e7
detbin <116663162+detbin@users.noreply.github.com>
2022-12-23 18:10:17 +0100

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,7 @@
+const family=['ipi','yaye','yaya','pa','bela']
+const familyset= new Set(family)
+console.log(family)
+familyset.add('pa')
+console.log(family)
+familyset.add('javaScript')
+console.log(familyset)
